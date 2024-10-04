c90fcb450c025afe889e9c16bd04846279134813
Marcos <marcos.eng.software@gmail.com>
2024-10-03 21:16:22 -0300

    Adicionando exercicios React Native

---
+++ mobile/react_native_dio/flexbox_responsividade/comandos_iniciais/App.tsx
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+4
+function CaixaRed() {
+  return <View
+    style={{
+      height: 400,
+      width: 100,
+      backgroundColor: 'red',
+      
+      
+    }}>
+
+  </View>
+}
+
+function CaixaBlue() {
+  return <View
+    style={{
+      height: 400,
+      width: 100,
+      backgroundColor: 'blue',
+      
+    }}>
+
+  </View>
+
+}
+
+function CaixaGreen() {
+  return <View
+    style={{
+      height: 400,
+      width: 100,
+      backgroundColor: 'green',
+      
+    }}>
+
+  </View>
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CaixaRed></CaixaRed>
+      <CaixaBlue></CaixaBlue>
+      <CaixaGreen></CaixaGreen>
+      <CaixaRed></CaixaRed>
+      <CaixaBlue></CaixaBlue>
+      <CaixaGreen></CaixaGreen>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    flex: 1,
+    flexWrap: 'wrap-reverse',
+    alignContent:'center',
+    borderWidth:5,
+    borderColor: 'orange',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    
+  },
+});

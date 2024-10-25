c3f2a0ef1c38976cc5810a7de5a49f24587d3d61
Marcos <marcos.eng.software@gmail.com>
2024-10-25 18:56:24 -0300

    Começando prijeto Pokedex

--- javaScript/JavaScript_exercicios_dio/exercicios/exercicio06.js
+++
@@ -1,6 +0,0 @@
-function escrevaMeuNome (nome) {
-    console.log('Meu nome é: ' + nome);
-}
-
-nome  = escrevaMeuNome('Marcos')
-nome  = escrevaMeuNome('Bond')
--- javaScript/JavaScript_exercicios_dio/exercicios/exercicio13.js
+++
@@ -1,9 +0,0 @@
-
-const entradas = [5,50,10,98,23];
-let i = 0;
-
-function gets() {
-    const valor = entradas[i];
-    i++
-}
-
